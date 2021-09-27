5febbd68e4f319d242e901b60a4bb106b73b9522
Moussel <moussel2011@gmail.com>
2021-09-27 19:07:27 +0200

    adding shoppin icon to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 import "./header.styles.scss";
 const Header = ({ currentUser }) => (
   <div className={"header"}>
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
