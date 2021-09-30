318b14ce7eecc6436b580fcbd6d9ac0e81e71962
Moussel <moussel2011@gmail.com>
2021-09-30 02:54:38 +0200

    update shopData file - add cart items to cart state - create add to cart function with quantity - connect cart icon  with cart items state

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 import CartIcon from "../cart-icon/cart-icon.component";
 import "./header.styles.scss";
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className={"header"}>
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -27,13 +28,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
-      <CartIcon />
+      {currentUser && <CartIcon />}
     </div>
+    {Boolean(currentUser) && !hidden && <CartDropDown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
