d078098660bb5cc575f065cbdf4ef13258fe582b
Moussel <moussel2011@gmail.com>
2021-10-01 18:15:21 +0200

    create a collection overview page -create directory state in redux -create shop state in redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { SHOP_DATA } from "./shop.data";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import CollectionsPreview from "../../components/collection-preview/collection-preview.component";
-class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollections }) => (
-          <CollectionsPreview key={id} {...otherCollections} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
